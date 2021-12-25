f60e8d9b671b82fcfc6b3a5b95af2f568ef4c873
matanna <mat.bonhommeau85@gmail.com>
2021-12-25 22:51:43 +0100

    Add sort system by popularity

--- public/js/listeners/sortMedia.js
+++ public/js/listeners/sortMedia.js
@@ -1,3 +1,5 @@
+import datas from '../../../data/datas.json' assert { type: "json" }
+
 export function sortMedia () {
   const options = document.querySelectorAll('.option')
   options.forEach((element) => {
@@ -9,15 +11,44 @@ export function sortMedia () {
 
 function chooseSort () {
   const sortType = this.dataset.value
-  const medias = document.querySelectorAll('.media-card')
+  let mediaSorted
 
-  const mediasTitle = Array.from(medias).map((element) => element.querySelector('.media-details__title').innerHTML).sort()
-  const mediasSorted = Array.from(medias).map((element) => {
-    mediasTitle.forEach((e) => {
-      if (e === element.querySelector('.media-details__title').innerHTML) {
-        return element
-      }
-    })
-  })
-  console.log(mediasSorted)
+  const medias = document.querySelectorAll('.media-card')
+  if (sortType === 'popularity') {
+    mediaSorted = Array.from(medias).sort((a, b) => b.querySelector('.media-details__likes').dataset.likes -
+                                                          a.querySelector('.media-details__likes').dataset.likes)
+  }
+
+  if (sortType === 'date') {
+    mediaSorted = Array.from(medias).sort((a, b) => {
+      const element = querySelector('.media-card')
+      if (element.querySelector('img')) {
+        return b.querySelector('.media-card > img').dataset.date -
+               a.querySelector('.media-card > img').dataset.date
+      } else if 
+      
+    }
+  }
+
+  const mediasSection = document.querySelector('.media-section')
+  mediaSorted.forEach((element) => mediasSection.append(element))
+
+  // const medias = datas.media.filter((element) => element.photographerId === parseInt(photographerId))
+
+
+
+  /*if (sortType === 'popularity') {
+    const mediaSorted = medias.sort((a, b) => b.likes - a.likes)
+    console.log(mediaSorted)
+  }
+
+  if (sortType === 'date') {
+    const mediaSorted = medias.sort((a, b) => a.date - b.date)
+    console.log(mediaSorted) 
+  }
+
+  if (sortType === 'title') {
+    const mediaSorted = medias.sort((a, b) => a.title < b.title ? -1 : 1)
+    console.log(mediaSorted)
+  }*/
 }
